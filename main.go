04686b84965f5f6fc39eb59f2a512f8c2a3f9995
Vlad Yelnikov <vlad.yelnikov@gmail.com>
2023-12-16 19:47:37 +0200

    add router

--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 	"net/http"
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "OK")
+func router(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintf(w, "OK")
+	case http.MethodPost:
+		fmt.Fprintf(w, "OK")
+	}
 }
 
 func main() {
-    const port string = "8080"
-    http.HandleFunc("/finance", get)
-    log.Println("Running on port", port)
-    err := http.ListenAndServe("localhost:8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	port := "8080"
+	http.HandleFunc("/", router)
+	log.Println("Running on port", port)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
