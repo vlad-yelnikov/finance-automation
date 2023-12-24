1b018bded14932d0ab8329914587f2f6df7af7d2
Vlad Yelnikov <vlad.yelnikov@gmail.com>
2023-12-24 19:59:06 +0200

    add body parser, add transaction struct

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+type Transaction struct {
+	ID              string  `json:"id"`
+	Time            int64   `json:"time"`
+	Description     string  `json:"description"`
+	MCC             int     `json:"mcc"`
+	OriginalMCC     int     `json:"originalMcc"`
+	Amount          int     `json:"amount"`
+	OperationAmount int     `json:"operationAmount"`
+	CurrencyCode    int     `json:"currencyCode"`
+	CommissionRate  float64 `json:"commissionRate"`
+	CashbackAmount  int     `json:"cashbackAmount"`
+	Balance         int     `json:"balance"`
+	Hold            bool    `json:"hold"`
+	ReceiptID       string  `json:"receiptId"`
+}
+
+func handleTransaction(w http.ResponseWriter, r *http.Request)  {
+	body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+		var transaction Transaction
+		unmarshalErr := json.Unmarshal(body, &transaction)
+		if unmarshalErr != nil {
+			log.Println(unmarshalErr)
+			return
+		}
+		fmt.Printf("%+v\n", transaction)
+		fmt.Fprint(w, "OK")
+}
+
 func router(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Fprintf(w, "OK")
+		fmt.Fprint(w, "OK")
 	case http.MethodPost:
-		fmt.Fprintf(w, "OK")
+		handleTransaction(w, r)
 	}
 }
 
